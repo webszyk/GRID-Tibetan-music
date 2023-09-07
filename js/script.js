8bc69e54fdd19ff54805c2571c9531166faea2ff
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2023-09-07 14:17:22 +0200

    header and menu update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const navMobile = document.querySelector(".nav-mobile");
+const navBtn = document.querySelector(".hamburger");
+const footerYear = document.querySelector(".footer__year");
+
+const handleNav = () => {
+  navBtn.classList.toggle("is-active");
+  navMobile.classList.toggle("nav-mobile--active");
+  document.body.classList.toggle("sticky-body");
+};
+
+navBtn.addEventListener("click", handleNav);
+
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
+
+handleCurrentYear();
